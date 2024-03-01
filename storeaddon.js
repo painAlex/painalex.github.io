982c3916e0b86a32257ecf4f3275af2ab8a9b897
painAlex <painalex1408@gmail.com>
2024-03-01 10:54:46 +0200

    Update storeaddon.js

--- storeaddon.js
+++ storeaddon.js
@@ -1938,19 +1938,7 @@ Lampa.SettingsApi.addParam({
                 }
 	
          });	
-/* Счётчик Яндекса */    
-	(function(m, e, t, r, i, k, a) {
-                       m[i] = m[i] || function() {
-                               (m[i].a = m[i].a || []).push(arguments)
-                       };
-                       m[i].l = 1 * new Date();
-                       for(var j = 0; j < document.scripts.length; j++) {
-                               if(document.scripts[j].src === r) {
-                                       return;
-                               }
-                       }
-                       k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-    })
+
     
 } // /* addonStart */
 if (!!window.appready) addonStart();
